f3bd71533980dcb8d26450d7437fd0728dc67e52
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 19:44:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0 
    return  "Buzz"
-  elsif int % 3 && 5 == 0 
+  elsif (int % 3 == 0 && int % 5 == 0)
     return  "FizzBuzz"
   elsif (int % 3 !=0) || (int % 5 !=0) 
     return nil 
