e573a7dbc0375c6868a75f4884008606b24c8faf
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 19:50:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
+require 'rspec'
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
