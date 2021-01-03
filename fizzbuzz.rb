858fa63e105b18038e40f37156a6eba116e9bf3e
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 19:16:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+     "Fizz"
   elsif int % 5 == 0 
-    puts "Buzz"
+     "Buzz"
   elsif (int % 3 == 0) && (int % 5 == 0)
-     puts "FizzBuzz"
+      "FizzBuzz"
   elsif (int % 3 !=0) || (int % 5 !=0) 
-    puts nil 
+     nil 
 end
 end  
