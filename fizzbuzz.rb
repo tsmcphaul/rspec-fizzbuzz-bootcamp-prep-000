b778f7eaa7769e3d37136a8d90d1bf332b818927
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 20:11:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,24 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    return "Fizz"
+  if (num % 3 ==0) && (num % 5 ==0)
+    return "FizzBuzz"
   elsif num % 5 == 0 
    return  "Buzz"
-  elsif num % 3 ==0 && num % 5 ==0
-    return  "FizzBuzz"
+  elsif num % 3 == 0 
+    return  "Fizz"
   elsif (num % 3 !=0) || (num % 5 !=0) 
     return nil 
 end
 end  
-def fizzbuzz(num)
-  if (num % 5 == 0) && (num % 3 == 0)
-    return "FizzBuzz"
-  elsif num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  else
-    return
-  end
-end
