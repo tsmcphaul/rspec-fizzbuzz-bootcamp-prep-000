9a1df028534b69953b1efa4ff1d9cf2e16594df0
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 18:42:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(integer)
+  if integer % 3 == 0
+    puts "Fizz"
+  elsif integer % 5 == 0 
+    puts "Buzz"
+  elsif integer % 3 && integer % 5 == 0
+    puts "Fizzbuzz"
+  elsif integer  !% 3||5 == 0
+    puts "nil"
+end  
