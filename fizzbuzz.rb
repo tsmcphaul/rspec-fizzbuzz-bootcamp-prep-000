adfaee6f2025bece2dae875ab05a481ff38cbd9c
proactive-callee-1012 <shaemc22@gmail.com>
2021-01-03 19:03:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
 def fizzbuzz(integer)
   if integer % 3 == 0
     puts "Fizz"
@@ -8,5 +9,5 @@ def fizzbuzz(integer)
   elsif (integer % 3 == 0) && (integer % 5 == 0)
     puts "FizzBuzz"
   elsif (integer % 3 !=0) || (integer % 5 !=0) 
-    puts "nil"
+
 end  
